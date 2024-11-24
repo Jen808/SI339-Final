727bdafa5605c3ea4b159ff3f8a8b8584bff4962
Jennifer Jung <chorong@umich.edu>
2024-11-23 22:23:08 -0500

    noview

--- script.js
+++ script.js
@@ -4,16 +4,9 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const pageloads = [
 
-        // { id: 'f1', url: 'AboutMe.html' },
-        // { id: 'f2', url: 'chs.html' },
-        // { id: 'f3', url: 'art.html' },
-        // { id: 'f4', url: 'src/Resume-CV.pdf' },
-        // { id: 's1', url: 'jenlio.html' },
-        { id: 'sec-mywiddy', url: 'my-widdy.html' },
+
         { id: 'sec-roosroast', url: 'roosroast.html' },
         { id: 'sec-glansis', url: 'glansis.html' },
-        { id: 'sec-vendors', url: 'vendors.html' },
-        { id: 'sec-ife', url: 'ife.html' },
         { id: 'sec-livmate', url: 'LivMate.html' },
 
     ];
@@ -34,171 +27,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
-
-
-// const bgGroup = document.querySelector('.svg-bg')
-// const bgPaths = bgGroup.querySelectorAll('path')
-// bgPaths.forEach((path, i) => {
-//     const length = path.getTotalLength()
-//     path.style.setProperty('--length', length)
-//     path.style.setProperty('--duration', length + 'ms')
-//     path.style.setProperty('--delay', i * 100 + 'ms')
-// })
-
-// const mainGroup = document.querySelector('.svg-main')
-// const mainPaths = mainGroup.querySelectorAll('path')
-// mainPaths.forEach((path, i) => {
-//     const length = path.getTotalLength()
-//     path.style.setProperty('--length', length)
-//     path.style.setProperty('--duration', length + 'ms')
-//     path.style.setProperty('--delay', i * 100 + 'ms')
-// })
-
-
-// function paragraph(element) {
-//     const array = element.innerText.split('')
-//     const special = ['~', '@', '!', '#', '$', '%', '^', '&', '*']
-//     const exception = [' ', '\n', '.', ',']
-//     const random = (min, max) => {
-//         return Math.floor(Math.random() * (max - min + 1) + min)
-//     }
-
-//     const numArray = []
-//     array.forEach(char => {
-//         const num = random(1, 10)
-//         numArray.push(num)
-//     })
-
-//     let completeCount
-//     let newText
-//     const timer = setInterval(() => {
-//         completeCount = 0
-//         newText = ''
-//         numArray.forEach((num, i) => {
-//             if (exception.includes(array[i]) || numArray[i] === 0) {
-//                 newText += array[i]
-//                 completeCount += 1
-//             } else {
-//                 newText += special[numArray[i] % special.length]
-//                 numArray[i] = --num
-//             }
-//         })
-
-//         element.innerText = newText
-//         if (completeCount === numArray.length) clearInterval(timer)
-//     }, 100)
-// }
-
-// const p = document.querySelector('.index-intro')
-// paragraph(p)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// class CardFlipOnScroll {
-//     constructor(wrapper, sticky) {
-//         this.wrapper = wrapper
-//         this.sticky = sticky
-//         this.cards = sticky.querySelectorAll('.card')
-//         this.length = this.cards.length
-
-//         this.start = 0
-//         this.end = 0
-//         this.step = 0
-//     }
-
-//     init() {
-//         this.start = 0
-//         this.end = this.wrapper.offsetTop + this.wrapper.offsetHeight - innerHeight * 1.2
-//         this.step = (this.end - this.start) / (this.length * 2)
-//     }
-
-//     animate() {
-//         this.cards.forEach((card, i) => {
-//             const s = this.start + this.step * i
-//             const e = s + this.step * (this.length + 1)
-
-//             if (scrollY <= s) {
-//                 card.style.transform = `
-//     perspective(100vw)
-//     translateX(100vw) 
-//     rotateY(180deg)
-//   `
-//             } else if (scrollY > s && scrollY <= e - this.step) {
-//                 card.style.transform = `
-//     perspective(100vw)
-//     translateX(${100 + (scrollY - s) / (e - s) * -100}vw)
-//     rotateY(180deg)
-//   `
-//             } else if (scrollY > e - this.step && scrollY <= e) {
-//                 card.style.transform = `
-//     perspective(100vw)
-//     translateX(${100 + (scrollY - s) / (e - s) * -100}vw)
-//     rotateY(${180 + -(scrollY - (e - this.step)) / this.step * 180}deg)
-//   `
-//             } else if (scrollY > e) {
-//                 card.style.transform = `
-//     perspective(100vw)
-//     translateX(0vw) 
-//     rotateY(0deg)
-//   `
-//             }
-//         })
-//     }
-// }
-
-// const mainContent1 = document.querySelector('.card-bg')
-// const sticky = document.querySelector('.sticky')
-// const cardFlipOnScroll = new CardFlipOnScroll(mainContent1, sticky)
-// cardFlipOnScroll.init()
-
-// window.addEventListener('scroll', () => {
-//     cardFlipOnScroll.animate()
-// })
-
-// window.addEventListener('resize', () => {
-//     cardFlipOnScroll.init()
-// })
-
-
-
-// const observer = new IntersectionObserver((entries) => {
-//     entries.forEach((entry) => {
-//         console.log(entry)
-//         if (entry.isIntersecting) {
-//             entry.target.classList.add('show');
-//         } 
-//     });
-// }, {
-//     threshold: 1
-// });
-
-
-
-// const hiddenDes = document.querySelectorAll('.des-box');
-// hiddenDes.forEach((el) => observer.observe(el));
-
-
-// const hiddenPj = document.querySelectorAll('.pj');
-// hiddenPj.forEach((el) => observer.observe(el));
-
-
-// const hiddensec = document.querySelectorAll('.sec');
-// hiddensec.forEach((el) => observer.observe(el));
-
-// const hiddensecing = document.querySelectorAll('.sec-ing');
-// hiddensecing.forEach((el) => observer.observe(el));
 
 
 
@@ -241,9 +69,3 @@ document.head.appendChild(link);
 
 
 
-// const customCursor = document.querySelector('.custom-cursor');
-
-// document.addEventListener('mousemove', (e) => {
-//   customCursor.style.left = `${e.pageX}px`;
-//   customCursor.style.top = `${e.pageY}px`;
-// });
